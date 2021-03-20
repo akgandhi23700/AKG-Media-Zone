0bee5b4ade0d1f5a4b199d994de509fa84d71dae
Amit Kumar gandhi <36218906+akgandhi23700@users.noreply.github.com>
2021-03-20 16:06:12 +0530

    Update and rename akgmediazone003.js to akgmediazone004.js

--- js/akgmediazone004.js
+++ js/akgmediazone004.js
@@ -177,16 +177,12 @@ function list(path) {
   </div>
   </div>
  <div class="card">
-    <h5>  
-	<center>
-<script type="text/javascript" language="javascript">
-      var aax_size='728x90';
+    <script type="text/javascript" language="javascript">
+      var aax_size='300x250';
       var aax_pubname = 'akgandhi23700-21';
       var aax_src='302';
     </script>
     <script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
-</center>
-    </h5>
   <div id="readme_md" style="display:block; padding: 100% 100%;"></div>
   </div>
   </div>
