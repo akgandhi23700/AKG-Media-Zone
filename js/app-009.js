c8e2b2f4094c79fa0fac214f94c0ce8425afc38e
Amit Kumar gandhi <36218906+akgandhi23700@users.noreply.github.com>
2021-04-25 12:51:51 +0530

    Update and rename app-008.js to app-009.js

--- js/app-009.js
+++ js/app-009.js
@@ -103,7 +103,7 @@ function nav(path) {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="/${cur}:/">Home</a>
+        <a class="nav-link">Home</a>
       </li>`;
     
     if (!model.is_search_page) {
