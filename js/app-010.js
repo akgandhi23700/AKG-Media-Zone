bf7d0a900f96e93762115e4caedeed37374305c4
Amit Kumar gandhi <36218906+akgandhi23700@users.noreply.github.com>
2021-04-25 12:54:04 +0530

    Update and rename app-009.js to app-010.js

--- js/app-010.js
+++ js/app-010.js
@@ -100,8 +100,8 @@ function nav(path) {
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+  <div>
+    <ul>
       <li class="nav-item">
         <a class="nav-link">Home</a>
       </li>`;
