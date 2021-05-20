0d84a67306e3a5617264069745af6150a8fa0d2e
Amit Kumar gandhi <36218906+akgandhi23700@users.noreply.github.com>
2021-05-20 18:20:13 +0530

    Update and rename app113.js to app114.js

--- js/app114.js
+++ js/app114.js
@@ -286,21 +286,6 @@ function list(path) {
   content += `</ol>
   </nav>
   </div>
-  
-    <div id="list">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-     <!-- akg--zone_sidebar_AdSense2_1x1_as -->
-     <ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2168199802246777"
-     data-ad-slot="7607238452"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-     <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-     </script>
-    </div>
-  
     <div id="list" class="list-group">
     </div>
   	<div class="${UI.file_count_alert_class} text-center d-none" role="alert" id="count">Total <span class="number text-center"></span> items</div>
@@ -540,6 +525,19 @@ function render_search_result_list() {
   <div class="card">
   <div class="${UI.path_nav_alert_class} d-flex align-items-center" role="alert" style="margin-bottom: 0;">Search Results</div>
   <div id="list" class="list-group">
+  <div>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- akg--zone_sidebar_AdSense2_1x1_as -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2168199802246777"
+     data-ad-slot="7607238452"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  </div>
   </div>
   </div>
   <div class="${UI.file_count_alert_class} text-center d-none" role="alert" id="count">Total <span class="number text-center"></span> items</div>
