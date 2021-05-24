d50cdcf1913ee1b797614f4e2741913808cc58d2
Amit Kumar gandhi <36218906+akgandhi23700@users.noreply.github.com>
2021-05-24 21:22:39 +0530

    Update akapps6.js

--- js/akapps6.js
+++ js/akapps6.js
@@ -209,7 +209,7 @@ function nav(path) {
     }
 
     html += `</div></li><li class="nav-item">
-    <a class="nav-link" href="${UI.contact_link}" target="_blank">Contact</a>
+    <a class="nav-link" href="${UI.contact_link}" target="_blank">Request Anything</a>
   </li>`;
 
     var search_text = model.is_search_page ? (model.q || '') : '';
